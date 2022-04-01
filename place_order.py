37ca5d4ab89e09df5c21983e43855d6a0067b633
Lim-Yi-Kiat <89069748+Lim-Yi-Kiat@users.noreply.github.com>
2022-04-01 13:00:31 +0800

    inventory CMS

--- place_order.py
+++ place_order.py
@@ -13,7 +13,7 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-inventory_URL = "http://localhost:5002/update_inventory"
+inventory_CMS = "http://localhost:5100/inventory_management"
 order_URL = "http://localhost:5001/order"
 # shipping_record_URL = "http://localhost:5002/shipping_record"
 # activity_log_URL = "http://localhost:5003/activity_log"
@@ -61,21 +61,16 @@ def place_order():
 
 
 def processPlaceOrder(order):
-    # 2. Send the order info {cart items}
-    # Invoke the order microservice
+
+    # 1. Send the order info {cart items}
+#################################################################### Invoke the order microservice ######################################################################################################
     print('\n-----Invoking order microservice-----')
     order_result = invoke_http(order_URL, method='POST', json=order)
     print('order_result:', order_result)
-
-        # 4. Record new order
-    # record the activity log anyway
-    print('\n\n-----Invoking inventory_URL microservice-----')
-    inventory_result = invoke_http(inventory_URL, method="POST", json=order)
-    print("\nOrder sent to inventory_URL log.\n")
-    code = order_result["code"]
+    code = order_result["code"]##################################### place this at the last 
     message = json.dumps(order_result)
     
-
+############################################## Error Handling #############################################################
     if code not in range(200, 300):
         # Inform the error microservice
         #print('\n\n-----Invoking error microservice as order fails-----')
@@ -99,34 +94,82 @@ def processPlaceOrder(order):
             "message": "Order creation failure sent for error handling."
         }
 
-    # Notice that we are publishing to "Activity Log" only when there is no error in order creation.
-    # In http version, we first invoked "Activity Log" and then checked for error.
-    # Since the "Activity Log" binds to the queue using '#' => any routing_key would be matched 
-    # and a message sent to “Error” queue can be received by “Activity Log” too.
 
+############################################################# No error Proceed to Inventory Check & send telegram if stock low ################################################
     else:
-        # 4. Record new order
-        # record the activity log anyway
-        #print('\n\n-----Invoking activity_log microservice-----')
-        print('\n\n-----Publishing the (order info) message with routing_key=order.info-----')        
-
-        # invoke_http(activity_log_URL, method="POST", json=order_result)            
+        print('\n\n-----Publishing the (order info) message with routing_key=order.info-----')               
         amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="order.info", 
             body=message)  
-        #
-    print('\n\n-----Invoking telegram bot microservice-----')
-    print(inventory_result)
-    bouquetDetail=inventory_result['data']['Details']
-    bouquetQuantity=  abs(inventory_result['data']["Quantity"])
+    # print('\n\n-----Invoking telegram bot microservice-----')
+    # print(inventory_result)
+    # bouquetDetail=inventory_result['data']['Details']
+    # bouquetQuantity=  abs(inventory_result['data']["Quantity"])
 
-    #if else check to see whether bouquetQuantity remaining is less than 50
-    if bouquetQuantity<50:
-        telegram_bot_sendtext(bouquetDetail,bouquetQuantity)
-    # - reply from the invocation is not used;
-    # continue even if this invocation fails
+    # #if else check to see whether bouquetQuantity remaining is less than 50
+    # if bouquetQuantity<50:
+    #     telegram_bot_sendtext(bouquetDetail,bouquetQuantity)
+    # # - reply from the invocation is not used;
+    # # continue even if this invocation fails
 
 
-    # 4. Record new order
+
+################################################################ 2. Send order to inventory management CMS ##################################################################
+    print('\n\n-----Invoking inventory_URL microservice-----')
+    inventory_result = invoke_http(inventory_CMS, method="POST", json=order)
+    print("\nOrder sent to inventory_URL log.\n")
+    print('inventory_result:', inventory_result)
+    code = order_result["code"]##################################### place this at the last 
+    message = json.dumps(order_result)
+
+############################################## Error Handling #############################################################
+    if code not in range(200, 300):
+        # Inform the error microservice
+        #print('\n\n-----Invoking error microservice as order fails-----')
+        print('\n\n-----Publishing the (order error) message with routing_key=order.error-----')
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="order.error", 
+            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+
+        # - reply from the invocation is not used;
+        # continue even if this invocation fails        
+        print("\nOrder status ({:d}) published to the RabbitMQ Exchange:".format(
+            code), order_result)
+
+        # 7. Return error
+        return {
+            "code": 500,
+            "data": {"inventory_result": inventory_result},
+            "message": "inventory_result failure sent for error handling."
+        }
+
+
+
+
+
+
+
+############################################### success return ############################################
+    return {
+            "code": 201,
+        "data": {
+            "order_result": order_result,
+            "inventory_result": inventory_result
+        }
+
+    }
+
+
+# Execute this program if it is run as a main script (not by 'import')
+# if __name__ == "__main__":
+#     print("This is flask " + os.path.basename(__file__) +
+#           " for placing an order...")
+#     app.run(host="0.0.0.0", port=5100, debug=True)
+
+if __name__ == '__main__':
+
+    app.run(port=5000, debug=True)
+
+    
+ # 4. Record new order
     # record the activity log anyway
     # print('\n\n-----Invoking activity_log microservice-----')
     # invoke_http(activity_log_URL, method="POST", json=order_result)
@@ -190,33 +233,3 @@ def processPlaceOrder(order):
 
 
     # 7. Return created order, shipping record
-
-    return {
-            "code": 201,
-        "data": {
-            "order_result": order_result,
-            # "shipping_result": shipping_result
-        }
-
-    }
-
-
-# Execute this program if it is run as a main script (not by 'import')
-# if __name__ == "__main__":
-#     print("This is flask " + os.path.basename(__file__) +
-#           " for placing an order...")
-#     app.run(host="0.0.0.0", port=5100, debug=True)
-
-if __name__ == '__main__':
-
-    app.run(port=5000, debug=True)
-
-    
-    # Notes for the parameters:
-    # - debug=True will reload the program automatically if a change is detected;
-    #   -- it in fact starts two instances of the same flask program,
-    #       and uses one of the instances to monitor the program changes;
-    # - host="0.0.0.0" allows the flask program to accept requests sent from any IP/host (in addition to localhost),
-    #   -- i.e., it gives permissions to hosts with any IP to access the flask program,
-    #   -- as long as the hosts can already reach the machine running the flask program along the network;
-    #   -- it doesn't mean to use http://0.0.0.0 to access the flask program.
